f19fbcbcb5bc8c0fa46c3f6203959fab1f7152c2
evelyn gomez <evelyngomezfrez@gmail.com>
2021-04-03 17:58:57 -0300

    routes, controller and views created.MVC Model, Views, Controller created, then theirs methods, basic authentication,user and password. Errors.any? for action posts/create in app/view.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,10 @@
 class PostsController < ApplicationController
   http_basic_authenticate_with name: "desafiovamoscontodo", password: "XAHTJEAS23123%23", only: :dashboard
+  
   def index
     @posts = Post.all.order(created_at: :desc)
+    #para mostrar de manera de descendente según fecha de creación
+    #se itera en Views/index.html.erb
   end
 
   def dashboard
@@ -9,5 +12,6 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.create(title: params[:title], image_url: params[:image_url], content: params[:content])
+    #se instancia y se crean los params para verlo en la url y en la base de datos
   end
 end
